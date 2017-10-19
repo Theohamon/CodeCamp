919b31c9f202553f75823339e0693e30858c0f85
Theohamon <theohamon9@gmail.com>
2017-10-19 20:18:27 +0200

    4e Jour Bis
    Ajout d'effet sur les covers
    + Ajouts de pages
    + Ajouts de plusieurs API

--- script.js
+++ script.js
@@ -14,3 +14,44 @@ $.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/musics?api_key=keyu2Ub4
             });
 					}
 );
+
+
+
+$.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/playlists?api_key=keyu2Ub4GvFkI7UKc",
+					function(data) {
+          	console.log(data);
+            $.each(data.records, function(key,val){
+
+ $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#playlists');
+
+            });
+					}
+);
+
+
+
+$.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/albums?api_key=keyu2Ub4GvFkI7UKc",
+					function(data) {
+          	console.log(data);
+            $.each(data.records, function(key,val){
+
+ $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#albums');
+
+            });
+					}
+);
+
+
+
+
+
+$.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/playlists2?api_key=keyu2Ub4GvFkI7UKc",
+					function(data) {
+          	console.log(data);
+            $.each(data.records, function(key,val){
+
+ $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#playlists2');
+
+            });
+					}
+);
