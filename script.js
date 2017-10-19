80b51c546a7b4a27ee80dda87498c93c4889f10c
Theohamon <theohamon9@gmail.com>
2017-10-19 15:18:04 +0200

    4eme Jour code
    Ajout de lien pour page article et concert
    + API

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ $.getJSON("https://api.airtable.com/v0/appFRdRpDLrXwRAtf/musics?api_key=keyu2Ub4
 					function(data) {
           	console.log(data);
             $.each(data.records, function(key,val){
-              console.log(val.fields.title);
 
-              $("<li>"+val.fields.title+"</li>").appendTo("#musiques");
+ $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#toplistapi');
+
             });
 					}
 );
